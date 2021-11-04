8edb00d524a911571c088321484b4a0503f4d67b
Pavel Yadlouski <pyadlous@redhat.com>
2021-11-04 20:11:02 +0100

    update batch implementation

--- calculators/BatchMandelCalculator.cc
+++ calculators/BatchMandelCalculator.cc
@@ -43,14 +43,14 @@ BatchMandelCalculator::~BatchMandelCalculator() {
 
 
 int *BatchMandelCalculator::calculateMandelbrot() {
-	int *pdata = data;
-    for (int block_line = 0; block_line < height / block_size; block_line++) {
+    for (int block_line = 0; block_line < height; block_line++) {
+        float initial_imag = y_start + block_line * dy;
+        
         for (int block_column = 0; block_column < width / block_size; block_column++) {
-            for (int i = 0; i < block_size; i++) {
-                float initial_imag = y_start + (block_size * block_line + i) * dy;
                 gt_4 = true;
-                for (int iter = 0; gt_4 && (iter < limit); iter++) {
-					
+                int *pdata = &data[block_line * width + block_column * block_size];
+                
+                for (int iter = 0; gt_4 && (iter < limit); iter++) {			
                     #pragma omp simd reduction(|:gt_4) simdlen(64)
 					for (int j = 0; j < block_size; j++) {
                         float initial_real =
@@ -64,15 +64,13 @@ int *BatchMandelCalculator::calculateMandelbrot() {
                         float i2 = current_img * current_img;
                         float r2 = current_real * current_real;
                         // if greater then 4 and cell is not set
-						int index = (block_line * block_size * width + i * width) + (block_column * block_size + j);
-                        bool gt = ((r2 + i2) > 4.0f) &&
-                                  (pdata[index] == limit);
-                        gt_4 |= gt;
+                        gt_4 |= ((r2 + i2) > 4.0f);
 
                         // update values from the next iteration with values
                         // form current iteration
-                        if (gt) {
-                            pdata[index] = iter;
+                        if ((r2 + i2) > 4.0f) {
+                            if ((pdata[j] == limit))
+                                pdata[j] = iter;
                         } else {
                             current_img_vec[j] =
                                 2.0f * current_img * current_real +
@@ -84,6 +82,5 @@ int *BatchMandelCalculator::calculateMandelbrot() {
             }
         }
         // this value is used in current iteration and do not change
-    }
     return data;
 }
