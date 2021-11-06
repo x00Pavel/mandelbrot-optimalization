8387de9a572efec981db777fcfecc1fe81f61620
x00Pavel <pavel.yadlouski@gmail.com>
2021-11-06 15:41:31 +0100

    Update line impl

--- calculators/LineMandelCalculator.cc
+++ calculators/LineMandelCalculator.cc
@@ -19,10 +19,14 @@ LineMandelCalculator::LineMandelCalculator(unsigned matrixBaseSize,
                                            unsigned limit)
     : BaseMandelCalculator(matrixBaseSize, limit, "LineMandelCalculator") {
     data = (int*)(malloc(height * width * sizeof(int))); // allocate aligned memory
-    current_real_vec = (float *)(malloc(width * sizeof(float)));  
-    current_img_vec = (float *)(malloc(width * sizeof(float)));
-    for (int i = 0; i < width * height; i++) {
-        data[i] = limit;
+    current_real_vec = (float *)(malloc(height * width * sizeof(float)));  
+    current_img_vec = (float *)(malloc(height * width * sizeof(float)));
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++){
+            data[i * width + j] = limit;
+            current_real_vec[i * width + j] = x_start + j * dx;
+            current_img_vec[i * width + j] = y_start + i * dy;
+        }
     }
 }
 
@@ -49,13 +53,13 @@ int* LineMandelCalculator::calculateMandelbrot()
         bool gt_4 = true;
 
         for (int iter = 0; gt_4 && (iter < limit); iter++) {
-#pragma omp simd reduction(| : gt_4) simdlen(64)
+#pragma omp simd reduction(| : gt_4) simdlen(64) aligned(pdata:64)
             for (int j = 0; j < width; j++) {
+    
                 float initial_real = x_start + j * dx;
-                float current_real =
-                    (iter == 0) ? initial_real : current_real_vec[j];
-                float current_img =
-                    (iter == 0) ? initial_imag : current_img_vec[j];
+                int index = i * width + j;
+                float current_real = current_real_vec[index];
+                float current_img = current_img_vec[index];
 
                 float i2 = current_img * current_img;
                 float r2 = current_real * current_real;
@@ -64,12 +68,11 @@ int* LineMandelCalculator::calculateMandelbrot()
 
                 // update values from the next iteration with values form
                 // current iteration
-                if ((r2 + i2) > 4.0f) {
-                    if ((pdata[j + i * width] == limit))
-                        pdata[i * width + j] = iter;
+                 if ((r2 + i2) > 4.0f && (pdata[index] == limit)) {
+                        pdata[index] = iter;
                 } else {
-                    current_img_vec[j] = 2.0f * current_img * current_real + initial_imag;
-                    current_real_vec[j] = r2 - i2 + initial_real;
+                    current_img_vec[index] = 2.0f * current_img * current_real + initial_imag;
+                    current_real_vec[index] = r2 - i2 + initial_real;
                 }
             }
         }
