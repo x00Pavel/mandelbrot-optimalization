ef1644da98f16aa793fadecf430365d3a03bbd92
Pavel Yadlouski <pyadlous@redhat.com>
2021-11-09 17:41:33 +0100

    Still Not vectorized

--- calculators/BatchMandelCalculator.cc
+++ calculators/BatchMandelCalculator.cc
@@ -26,11 +26,15 @@ BatchMandelCalculator::BatchMandelCalculator(unsigned matrixBaseSize,
                                         block_size));
     initial_imag = (float *)(_mm_malloc(block_size * block_size * sizeof(float),
                                         block_size));
-    current_real_vec = (float *)(_mm_malloc(
-        block_size * block_size * sizeof(float), block_size));
-    current_img_vec = (float *)(_mm_malloc(
-        block_size * block_size * sizeof(float), block_size));
-
+    // current_real_vec = (float *)(_mm_malloc(
+    //     block_size * block_size * sizeof(float), block_size));
+    // current_img_vec = (float *)(_mm_malloc(
+    //     block_size * block_size * sizeof(float), block_size));
+    current_real_vec =
+        (float *)(_mm_malloc(block_size * sizeof(float), block_size));
+    current_img_vec =
+        (float *)(_mm_malloc(block_size * sizeof(float), block_size));
+    line_done = (bool *)(_mm_malloc(block_size * sizeof(bool), block_size));
     int i;
 
 #pragma omp simd linear(data : 1) simdlen(128)
@@ -55,61 +59,65 @@ BatchMandelCalculator::~BatchMandelCalculator() {
     _mm_free(initial_real);
     initial_real = NULL;
 
+    _mm_free(line_done);
+    line_done = NULL;
+
 }
 
 int *BatchMandelCalculator::calculateMandelbrot() {
-    // int *pdata = data;
+    int *pdata = data;
     // Split the number of rows of the C matrix into a few blocks
     for (size_t blockN = 0; blockN < height / block_size; blockN++) {
         // Split the number of cols of the C matrix into a few blocks
         for (size_t blockP = 0; blockP < width / block_size; blockP++) {
             // Initialize init values for next block
 
-#pragma omp simd simdlen(128) linear(initial_real : 1) linear(initial_imag : 1)
-            for (int k_line = 0; k_line < block_size; k_line++) {
-                for (int k_row = 0; k_row < block_size; k_row++) {
-                    initial_real[k_line * block_size + k_row] =
-                        x_start + (blockP * block_size + k_row) * dx;
-                    initial_imag[k_line * block_size + k_row] =
-                        y_start + (blockN * block_size  + k_line) * dy;
-                }
-            }
-            int *pdata = &data[blockN * block_size * width +
-                              blockP * block_size];
+// #pragma omp simd simdlen(128) linear(initial_real : 1) linear(initial_imag : 1)
+//             for (int k_line = 0; k_line < block_size; k_line++) {
+//                 for (int k_row = 0; k_row < block_size; k_row++) {
+//                     initial_real[k_line * block_size + k_row] =
+//                         x_start + (blockP * block_size + k_row) * dx;
+//                     initial_imag[k_line * block_size + k_row] =
+//                         y_start + (blockN * block_size  + k_line) * dy;
+//                 }
+//             }
+            // int *pdata = &data[blockN * block_size * width +
+            //                   blockP * block_size];
 
             for (int i = 0; i < block_size; i++) {
                 const int iGlobal = blockN * block_size + i;
-
+                const float imag = y_start + iGlobal * dy;
                 bool gt_4 = false;
 
+#pragma omp simd simdlen(128)
+                for (int k = 0; k < block_size; k++){
+                    current_real_vec[k] = x_start + (blockP * block_size + k) * dx;
+                    current_img_vec[k] = imag;
+                }
+
                 for (int iter = 0; !gt_4 && (iter < limit); iter++) {
-#pragma omp simd reduction(& : gt_4) simdlen(128) aligned(pdata : 128) linear(update:1)
+#pragma omp simd reduction(& : gt_4) simdlen(128) aligned(pdata : 128)
                     for (int j = 0; j < block_size; j++) {
                         const int jGlobal = blockP * block_size + j;
-                        const int index = i * block_size + j;
-                        // const int indexGlobal = iGlobal * width + jGlobal;
+                        float real = x_start + jGlobal * dx;
 
-                        float current_real = (iter == 0)
-                                                 ? initial_real[index]
-                                                 : current_real_vec[index];
-                        float current_imag = (iter == 0)
-                                                 ? initial_imag[index]
-                                                 : current_img_vec[index];
+                        // float current_real =
+                        //     (iter == 0) ? real : current_real_vec[j];
+                        // float current_imag =
+                        //     (iter == 0) ? imag : current_img_vec[j];
 
-                        float r2 = current_real * current_real;
-                        float i2 = current_imag * current_imag;
+                        float r2 = current_real_vec[j] * current_real_vec[j];
+                        float i2 = current_img_vec[j] * current_img_vec[j];
 
                         gt_4 &= (r2 + i2 > 4.0f);
 
-                        if ((r2 + i2 > 4.0f) && pdata[index] == limit) {
-                            pdata[index] = iter;
+                        if (r2 + i2 > 4.0f && (pdata[iGlobal * width + jGlobal] == limit)) {
+                                pdata[iGlobal * width + jGlobal] = iter;
                         }
                         else{
-                            current_img_vec[index] =
-                                2.0f * current_imag * current_real +
-                                initial_imag[index];
-                            current_real_vec[index] =
-                                r2 - i2 + initial_real[index];
+                            current_img_vec[j] =
+                                2.0f * current_img_vec[j] * current_real_vec[j] + imag;
+                            current_real_vec[j] = r2 - i2 + real;
                         }
                     }
                 }
